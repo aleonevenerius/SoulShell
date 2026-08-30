a654653fdabd6aa4be97555296dc15ee81af0d4d
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-30 18:39:15 -0300

    Home is done

---
+++ Main.py
@@ -0,0 +1,21 @@
+home = """
+#####################
+#                   #
+#     SOULSHELL     #
+# Just be your self #
+#                   #
+#####################
+
+Welcome to SoulShell - your personal diary via terminal.
+
+Please, choose any option beneath:
+
+1. Add a new chapter;
+2. View a chapter;
+3. Edit chapter;
+4. Delete a chapter;
+5. View all chapter;
+6. Exit.
+"""
+print(home)
+choose = int(input(">"))

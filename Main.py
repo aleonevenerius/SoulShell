0c3e3074c7dad42a3d38bf11931a5a7e5a3e0e77
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-31 16:11:06 -0300

    Loading was added

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import time
+
 home = """
 ######################
 #                    #
@@ -15,15 +17,48 @@ Please, choose any option beneath:
 3. Edit chapter;
 4. Delete a chapter;
 5. View all chapter;
-6. Exit.
+6. Create a new notebook;
+7. Delete a new notebook;
+8. Exit.
 """
+
 print(home)
+
+def ToTime():
+    icons = ["|", "/", "-", "\\", "|"]
+    i = 0
+    while i < 5:
+        print(f"\rLoading {icons[i%len(icons)]}", end="", flush=True)
+        i += 1
+        time.sleep(0.1)
+
+    time.sleep(1)
+    print("\nDone!")
 
 def ToChoose():
     try:
         c = int(input("> "))
-    
+
     except ValueError:
         print("There isn't this option.")
-        return 6
+        return 8
+
+    else:
+         match c:
+            case 1:
+                ToTime()
+            case 2:
+                ToTime()
+            case 3:
+                ToTime()
+            case 4:
+                ToTime()
+            case 5:
+                ToTime()
+            case 6:
+                ToTime()
+            case 7:
+                ToTime()
+            case 8:
+                ToTime()
 ToChoose()
